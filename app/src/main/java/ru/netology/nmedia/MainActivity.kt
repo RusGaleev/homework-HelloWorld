e6b1db2a448b5cad081cc307c5fa4246423eb6e8
Rus Galeev <spyder44@yandex.ru>
2022-05-07 13:11:57 +0300

    fix strings.xml

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,16 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val likeButton = findViewById<ImageButton>(R.id.post_favorite_button)
+        likeButton.setOnClickListener {
+            likeButton.setImageResource(R.drawable.ic_favorite_24dp)
+        }
     }
+
 }
