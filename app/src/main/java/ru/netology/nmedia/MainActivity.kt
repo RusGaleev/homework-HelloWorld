f22c2162b6712b4c3f6eb155896390baf0816b1d
Rus Galeev <spyder44@yandex.ru>
2022-05-08 12:50:39 +0300

    likesButton working, post on view

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -3,15 +3,42 @@ package ru.netology.nmedia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import androidx.annotation.DrawableRes
+import ru.netology.nmedia.data.Post
+import ru.netology.nmedia.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val likeButton = findViewById<ImageButton>(R.id.post_favorite_button)
-        likeButton.setOnClickListener {
-            likeButton.setImageResource(R.drawable.ic_favorite_24dp)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val post = Post(
+            id = 1,
+            author = "Sasha",
+            content = "Events",
+            published = "07.05.2022"
+        )
+
+        binding.render(post)
+        binding.postFavoriteButton.setOnClickListener {
+            post.likedByMe = !post.likedByMe
+            binding.postFavoriteButton.setImageResource(getLikeIconResId(post.likedByMe))
         }
     }
+
+    private fun ActivityMainBinding.render(post: Post) {
+        postAuthorName.text = post.author
+        postText.text = post.content
+        postDate.text = post.published
+        postFavoriteButton.setImageResource(getLikeIconResId(post.likedByMe))
+    }
+
+    @DrawableRes
+    private fun getLikeIconResId(liked: Boolean) =
+        if (liked) R.drawable.ic_favorite_24dp else R.drawable.ic_favorite_border_24dp
 
 }
---
+++ app/src/main/java/ru/netology/nmedia/data/Post.kt
@@ -0,0 +1,10 @@
+package ru.netology.nmedia.data
+
+data class Post(
+    val id: Long,
+    val author: String,
+    val content: String,
+    val published: String,
+    val likes:Int = 0,
+    var likedByMe: Boolean = false
+)
