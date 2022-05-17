4eb9b30d82bf44409450daf0d14371a21ccdb243
Rus Galeev <spyder44@yandex.ru>
2022-05-17 23:13:49 +0300

    on lecture, working

--- app/src/main/java/ru/netology/nmedia/viewModel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewModel/PostViewModel.kt
@@ -1,16 +1,41 @@
 package ru.netology.nmedia.viewModel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.netology.nmedia.adapter.PostInteractionsListener
 import ru.netology.nmedia.data.Post
 import ru.netology.nmedia.data.PostRepository
 import ru.netology.nmedia.data.impl.InMemoryPostRepository
 
-class PostViewModel:ViewModel() {
+class PostViewModel:ViewModel(), PostInteractionsListener {
     private val repository: PostRepository = InMemoryPostRepository()
 
     val data by repository::data
 
-    fun onLikeClicked(post:Post) = repository.like(post.id)
+    val currentPost = MutableLiveData<Post?>(null)
 
-    fun onShareClicked(post:Post) = repository.share(post.id)
+    fun onSaveButtonClick(content:String){
+        if (content.isBlank()) return
+
+        val newPost =currentPost.value?.copy(
+            content = content
+        )?: Post(
+            id = PostRepository.NEW_POST_ID,
+            author = "Me",
+            content = content,
+            published = "Today"
+        )
+        repository.save(newPost)
+        currentPost.value = null
+    }
+
+    // region PostInteractionsListener
+    override fun onShareClicked(post:Post) = repository.share(post.id)
+    override fun onLikeClicked(post:Post) = repository.like(post.id)
+    override fun onDeleteClicked(post:Post) = repository.delete(post.id)
+    override fun onEditClicked(post: Post) {
+       currentPost.value = post
+    }
+
+    // endregion PostInteractionsListener
 }
